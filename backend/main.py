93bd453ed2bc641c2ac4b720a4ce747220477982
mdatascience375-source <mdatascience375@gmail.com>
2025-12-12 13:42:05 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -471,4 +471,6 @@ def sales_monthly():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
